d73cdd6ac66a87bd99054cd9216eb7e65190f355
Felipe Martinello Fornari <felipe.fornari@gmail.com>
2019-05-16 11:16:14 -0300

    criadas classes model, repository, service, service/impl e controller para equipamento

---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/controller/EquipamentoController.java
@@ -0,0 +1,25 @@
+package br.edu.utfpr.chemistsincontrol.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.utfpr.chemistsincontrol.model.Equipamento;
+import br.edu.utfpr.chemistsincontrol.service.CrudService;
+import br.edu.utfpr.chemistsincontrol.service.EquipamentoService;
+
+@RestController
+@RequestMapping("equipamento")
+public class EquipamentoController extends CrudController<Equipamento, Long> {
+
+	@Autowired
+	private EquipamentoService equipamentoService;
+	
+	@Override
+	@Valid
+	protected CrudService<Equipamento, Long> getService() {
+		return equipamentoService;
+	}
+}
---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/service/EquipamentoService.java
@@ -0,0 +1,5 @@
+package br.edu.utfpr.chemistsincontrol.service;
+
+import br.edu.utfpr.chemistsincontrol.model.Equipamento;
+
+public interface EquipamentoService extends CrudService<Equipamento, Long>{}
