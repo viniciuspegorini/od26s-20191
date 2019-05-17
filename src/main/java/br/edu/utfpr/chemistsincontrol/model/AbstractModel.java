fb3ae8a11eb061a8f05a70568fad104cb503af68
Cleidisson <cleipd@gmail.com>
2019-05-17 20:22:25 -0300

    AbstractModel + extends

---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/model/AbstractModel.java
@@ -0,0 +1,24 @@
+package br.edu.utfpr.chemistsincontrol.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class AbstractModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
