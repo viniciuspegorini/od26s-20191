6e237c84006ca3640175bd1397592357b30bf127
Francisco Hansen <franciscohansen@alunos.utfpr.edu.br>
2019-05-17 20:52:44 -0300

    Alteração CrudController Criação da Configuração de Auditoria

--- src/main/java/br/edu/utfpr/chemistsincontrol/model/Usuario.java
+++ src/main/java/br/edu/utfpr/chemistsincontrol/model/Usuario.java
@@ -1,10 +1,16 @@
 package br.edu.utfpr.chemistsincontrol.model;
 
+import org.hibernate.validator.constraints.CodePointLength;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 public class Usuario extends AbstractModel {
-
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
 
 
 }
