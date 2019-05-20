4d30aee2b95fe8e544d192c63215ddbea998773c
Cleverson Fulber <cleversonfulber@alunos.utfpr.edu.br>
2019-05-20 19:53:49 -0300

    Commit de pessoa

---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/model/Pessoa.java
@@ -0,0 +1,62 @@
+package br.edu.utfpr.chemistsincontrol.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pessoa")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Data
+public class Pessoa extends AbstractModel {
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Nome'.")
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'CPF'.")
+    @Column(length = 100, nullable = false)
+    private String cpf;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Nome'.")
+    @Column(length = 100, nullable = false)
+    private String rg;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Nome'.")
+    @Column(length = 100, nullable = false)
+    private String telefone;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Nome'.")
+    @Column(length = 100, nullable = false)
+    private String celular;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Nome'.")
+    @Column(length = 100, nullable = false)
+    private String departamento;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Nome'.")
+    @Column(length = 100, nullable = false)
+    private String status;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Pessoa'.")
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id")
+    private Pessoa pessoa;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Pessoa'.")
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id")
+    private Instituicao instituicao;
+
+    @NotNull(message = "Não esqueça de preencher o campo 'Pessoa'.")
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id")
+    private Usuario usuario;
+
+}
---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/service/impl/PessoaServiceImpl.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr.chemistsincontrol.service.impl;
+
+import br.edu.utfpr.chemistsincontrol.model.Pessoa;
+import br.edu.utfpr.chemistsincontrol.repository.PessoaRepository;
+import br.edu.utfpr.chemistsincontrol.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaServiceImpl extends CrudServiceImpl<Pessoa>
+implements PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Override
+	protected PessoaRepository getRepository() {
+		return pessoaRepository;
+	}
+}
