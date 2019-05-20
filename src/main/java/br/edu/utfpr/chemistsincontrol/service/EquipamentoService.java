17d3c035ff692f98f94eb57180c2878333418976
Francisco Hansen <franciscohansen@alunos.utfpr.edu.br>
2019-05-20 18:52:09 -0300

    Commit Before Merge with C-eqp

--- src/main/java/br/edu/utfpr/chemistsincontrol/service/EquipamentoService.java
+++ src/main/java/br/edu/utfpr/chemistsincontrol/service/EquipamentoService.java
@@ -3,5 +3,5 @@ package br.edu.utfpr.chemistsincontrol.service;
 import br.edu.utfpr.chemistsincontrol.model.Equipamento;
 
 public interface EquipamentoService extends CrudService<Equipamento> {
-    
+
 }
