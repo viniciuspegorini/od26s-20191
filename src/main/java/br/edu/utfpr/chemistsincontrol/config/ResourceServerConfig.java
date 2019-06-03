c4f2a2abfc91dfa349c596f690b53c469040e633
Roberto Ramos <rtorms@gmail.com>
2019-06-03 19:50:04 -0300

    Tela login

---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/config/ResourceServerConfig.java
@@ -0,0 +1,26 @@
+package br.edu.utfpr.chemistsincontrol.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/equipamento/**").hasAnyRole("ADMIN,USER")
+			.antMatchers("/pessoa/**").hasAnyRole("ADMIN,USER")
+			.antMatchers("/modelo/**").hasAnyRole("ADMIN")
+			.antMatchers("/instituicao/**").hasAnyRole("ADMIN,USER")
+			.antMatchers("/login/**").permitAll()
+			.antMatchers("/usuario/**").permitAll()
+			.anyRequest().authenticated();
+	}
+}
---
+++ src/main/java/br/edu/utfpr/chemistsincontrol/config/WebMvcConfig.java
@@ -0,0 +1,46 @@
+package br.edu.utfpr.chemistsincontrol.config;
+
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private LocalDateSerializer localDateSerializer;
+    @Autowired
+    private LocalDateDeserializer localDateDeserializer;
+
+    private MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+        SimpleModule module = new SimpleModule("app", Version.unknownVersion());
+        module.addSerializer(LocalDate.class, localDateSerializer);
+        module.addDeserializer(LocalDate.class, localDateDeserializer);
+        objectMapper.registerModule(module);
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new StringHttpMessageConverter());
+        converters.add(customJackson2HttpMessageConverter());
+        converters.add(new FormHttpMessageConverter());
+    }
+
+}
