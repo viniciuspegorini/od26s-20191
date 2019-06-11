e1ee3dab405b536ac5b9f06a5ccdf93947f7c920
Cleverson Fulber <cleversonfulber@alunos.utfpr.edu.br>
2019-06-11 19:04:23 -0300

    Alteração das Permissões

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,7 +8,9 @@ insert into pessoa (id, created_by, created_date, last_modified_by, last_modifie
 insert into preco (id, created_by, created_date, last_modified_by, last_modified_date, tipo_pessoa, un_medida, valor, equipamento_id ) values (1,'TESTE', '2019-05-21 19:10:11.484', 'TESTE', '2019-05-21 19:10:11.484', 'Tipo Pessoa', 'Unidade', 10.5, 1 );
 
 insert into permissao (nome ) values ('ROLE_ADMIN' );
-insert into permissao (nome ) values ('ROLE_USER' );
+insert into permissao (nome ) values ('ROLE_SOLICITANTE' );
+insert into permissao (nome ) values ('ROLE_TECNICO' );
+insert into permissao (nome ) values ('ROLE_RECEPCIONISTA' );
 
 insert into usuario_permissoes( usuario_id, permissoes_id ) values ( 1, 1 );
 insert into usuario_permissoes( usuario_id, permissoes_id ) values ( 1, 2 );
