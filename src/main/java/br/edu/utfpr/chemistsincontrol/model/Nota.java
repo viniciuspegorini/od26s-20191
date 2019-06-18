9193efb90a069c20f6f9b40c656f1150bbeec931
Felipe Martinello Fornari <felipe.fornari@gmail.com>
2019-06-18 20:31:13 -0300

    Alterado pessoa/usuario conforme BD

--- src/main/java/br/edu/utfpr/chemistsincontrol/model/Nota.java
+++ src/main/java/br/edu/utfpr/chemistsincontrol/model/Nota.java
@@ -31,10 +31,10 @@ public class Nota extends AbstractModel{
     @Column(length = 100, nullable = false)
     private String tipoNota;
 	
-    @NotNull(message = "Não esqueça de preencher o campo 'Pessoa'.")
+    @NotNull(message = "Não esqueça de preencher o campo 'Usuário'.")
     @ManyToOne
     @JoinColumn(referencedColumnName = "id")
-    private Pessoa pessoa;
+    private Usuario usuario;
     
     @NotNull(message = "Opa!! Não esqueça de preencher o campo 'Valor'.")
     @Column(nullable = false)
--- src/main/java/br/edu/utfpr/chemistsincontrol/service/PessoaService.java
+++
@@ -1,7 +0,0 @@
-package br.edu.utfpr.chemistsincontrol.service;
-
-import br.edu.utfpr.chemistsincontrol.model.Pessoa;
-
-public interface PessoaService extends CrudService<Pessoa> {
-
-}
