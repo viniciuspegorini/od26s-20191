6e741b816cb2a097892ea4227921cee06edd2c17
Francisco Godienski Hansen <franciscohansen@alunos.utfpr.edu.br>
2019-07-09 19:54:19 -0300

    Revert "S03 mail"

--- src/main/java/br/edu/utfpr/chemistsincontrol/repository/ResultadoRepository.java
+++ src/main/java/br/edu/utfpr/chemistsincontrol/repository/ResultadoRepository.java
@@ -3,10 +3,6 @@ package br.edu.utfpr.chemistsincontrol.repository;
 import br.edu.utfpr.chemistsincontrol.model.Resultado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ResultadoRepository extends JpaRepository<Resultado, Long> {
-
-    Optional<Resultado> findByFormularioId(Long idFormulario);
 
 }
