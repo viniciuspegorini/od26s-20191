6b58870414001a8167ca78e37ab453027ccbc89c
Sendeskill <lucastsendeski@hotmail.com>
2019-07-11 19:22:06 -0300

    criação da consulta no formulario

--- src/main/java/br/edu/utfpr/chemistsincontrol/controller/FormularioController.java
+++ src/main/java/br/edu/utfpr/chemistsincontrol/controller/FormularioController.java
@@ -1,17 +1,15 @@
 package br.edu.utfpr.chemistsincontrol.controller;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
+import br.edu.utfpr.chemistsincontrol.model.Formulario;
+import br.edu.utfpr.chemistsincontrol.service.CrudService;
+import br.edu.utfpr.chemistsincontrol.service.FormularioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.utfpr.chemistsincontrol.model.Formulario;
-import br.edu.utfpr.chemistsincontrol.service.CrudService;
-import br.edu.utfpr.chemistsincontrol.service.FormularioService;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("formulario")
@@ -25,10 +23,14 @@ public class FormularioController extends CrudController<Formulario, Long> {
     protected CrudService<Formulario, Long> getService() {
         return formularioService;
     }
-    
+
     @GetMapping("/forms")
-    public List<Formulario> getForm(){
+    public List<Formulario> getForm() {
         return formularioService.getForm();
     }
-    
+
+    @GetMapping("/find/{id}")
+    public List<Formulario> findByNotaId(Long Id) {
+        return formularioService.findByNotaId(Id);
+    }
 }
