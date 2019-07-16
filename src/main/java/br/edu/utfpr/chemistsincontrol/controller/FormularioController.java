7e5e68ea93e98e79bd673c8bef7bb26537b4f569
Francisco Godienski Hansen <franciscohansen@alunos.utfpr.edu.br>
2019-07-15 22:07:14 -0300

    Update FormularioController.java

--- src/main/java/br/edu/utfpr/chemistsincontrol/controller/FormularioController.java
+++ src/main/java/br/edu/utfpr/chemistsincontrol/controller/FormularioController.java
@@ -1,17 +1,21 @@
 package br.edu.utfpr.chemistsincontrol.controller;
 
-
 import br.edu.utfpr.chemistsincontrol.model.Formulario;
 import br.edu.utfpr.chemistsincontrol.service.CrudService;
 import br.edu.utfpr.chemistsincontrol.service.FormularioService;
 import java.io.IOException;
-
 import java.util.List;
+import java.util.Optional;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
 
+import br.edu.utfpr.chemistsincontrol.model.Resultado;
+import br.edu.utfpr.chemistsincontrol.repository.ResultadoRepository;
+import br.edu.utfpr.chemistsincontrol.util.mail.MailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,20 +26,23 @@ import java.util.List;
 @RequestMapping("formulario")
 public class FormularioController extends CrudController<Formulario, Long> {
 
+    private static final String EMAIL_FUNTEF = "cicerafrozza.cf@gmail.com";
+
     @Autowired
     private FormularioService formularioService;
+    @Autowired
+    private ResultadoRepository resultadoRepository;
 
     @Override
     @Valid
     protected CrudService<Formulario, Long> getService() {
         return formularioService;
     }
-    
+
     @GetMapping("/forms")
-    public List<Formulario> getForm(){
+    public List<Formulario> getForm() {
         return formularioService.getForm();
     }
-
 
     @GetMapping("/find/{id}")
     public List<Formulario> findByNotaId(Long Id) {
